05886fdfcd9571e0d8654a460879852ef16f063b
Anton <antonkosicyn@rambler.ru>
2021-07-09 13:28:19 +0300

    начали вести историю

---
+++ cards.js
@@ -0,0 +1,62 @@
+const localCards = [{
+        rus: "Робко",
+        eng: "Humbly",
+        sentenceRus: "Я не знала, прости, сказала Полли робко",
+        sentenceEng: "I didn’t know. I’m sorry, said Polly humbly.",
+        audio: null,
+    }, {
+        rus: "Напрасно",
+        eng: "In vain",
+        sentenceRus: "...это только вызвало бы у тебя слюноотделение напрасно",
+        sentenceEng: "...it would only make your mouth water in vain.",
+        audio: null,
+    }, {
+        rus: "Карабкаться",
+        eng: "Scramble",
+        sentenceRus: "Однажды утром она была у себя в саду, когда мальчик вскарабкался на ее забор",
+        sentenceEng: "One morning she was out in the back garden when a boy scrambled up from the garden next door",
+        audio: null,
+    }, {
+        rus: "Привлекательный",
+        eng: "Attractive",
+        sentenceRus: "...особенно, осмелюсь сказать, с такой привлекательной юной леди, как вы",
+        sentenceEng: "...especially, if I may say so, such a very attractive young lady as yourself ",
+        audio: null,
+    },
+    {
+        rus: "Возможность",
+        eng: "Opportunity",
+        sentenceRus: "Это слишком хорошая возможность, чтобы ее упустить",
+        sentenceEng: "This is too good an opportunity to miss",
+        audio: null,
+    },
+    {
+        rus: "Слабый",
+        eng: "Faint",
+        sentenceRus: "Слышался слабый, очень-очень слабый гудящий звук",
+        sentenceEng: "There was a faint—a very, very faint—humming sound",
+        audio: null,
+    },
+    {
+        rus: "Ручка",
+        eng: "Handle",
+        sentenceRus: "На этой стороне не было ни засова, ни ручки",
+        sentenceEng: "There was no bolt or handle on this side of it ",
+        audio: null,
+    },
+    {
+        rus: "Тереть",
+        eng: "Rub",
+        sentenceRus: "Оно едва ли могло быть грязнее, даже если бы он сначала втирал в руки землю и затем ,сильно поплакав, измазал бы свое лицо грязными руками",
+        sentenceEng: "It could hardly have been grubbier if he had first rubbed his hands in the earth, and then had a good cry, and then dried his face with his hands",
+        audio: null,
+    },
+    {
+        rus: "Главным образом",
+        eng: "Сhiefly",
+        sentenceRus: "Их приключения начались главным образом благодаря тому, что это было самое мокрое и холодное лето за все последние годы",
+        sentenceEng: "Their adventures began chiefly because it was one of the wettest and coldest summers there had been for years.",
+        audio: null,
+    },
+
+]
---
+++ script.js
@@ -0,0 +1,86 @@
+const textArea = document.getElementById("textArea")
+const sentence = document.getElementById("sentence")
+const buttonElements = document.getElementsByTagName('button')
+
+const answerBtn = document.getElementById("answer-stop")
+const nextcardBtn = document.getElementById("next-card")
+const loader = document.getElementById("loader")
+const cardContainer = document.getElementById("card-container")
+const playBtn = document.getElementById("play-pause")
+
+const audioElement = document.getElementById('audio')
+
+let card
+let index = 0
+
+function showLoadingSpinner() {
+    loader.hidden = false
+    cardContainer.hidden = true
+}
+
+function removeLoadingSpinner() {
+    loader.hidden = true
+    cardContainer.hidden = false
+}
+
+function showAnswer() {
+
+    showLoadingSpinner()
+    answerBtn.innerHTML = 'Stop'
+    sentence.textContent = card.sentenceEng
+    textArea.textContent = card.eng
+    let word = card.eng.replace(/\s/g, '')
+    audioElement.src = 'audio1/' + word + '.mp3'
+    playBtn.hidden = false
+    removeLoadingSpinner()
+}
+
+function toggleButton() {
+
+    // playBtn.disabled = !playBtn.disabled
+    // answerBtn.disabled = !answerBtn.disabled
+
+    console.log(answerBtn.disabled)
+
+}
+
+function playTrack() {
+    audioElement.play()
+    toggleButton()
+}
+
+function checkIndex() {
+    if (index === localCards.length) {
+        index = 0
+    }
+}
+
+function generateWord() {
+    playBtn.disabled = false
+    answerBtn.disabled = false
+    answerBtn.innerHTML = 'Answer'
+        // console.log(answerBtn.disabled)
+        // toggleButton()
+    audioElement.pause()
+    showLoadingSpinner()
+    playBtn.hidden = true
+    card = localCards[index]
+        // card = localCards[Math.floor(Math.random() * localCards.length)]
+    textArea.textContent = card.rus
+    sentence.textContent = card.sentenceRus
+    if (card.sentenceRus.length > 120) {
+        sentence.classList.add("long-text")
+    } else {
+        sentence.classList.remove("long-text")
+    }
+    removeLoadingSpinner()
+    index++
+    checkIndex()
+}
+
+generateWord()
+
+answerBtn.addEventListener("click", showAnswer)
+nextcardBtn.addEventListener("click", generateWord)
+playBtn.addEventListener("click", playTrack)
+audioElement.addEventListener('ended', toggleButton)
