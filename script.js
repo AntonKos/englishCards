1906b1d921b579c0afac130fcd0a4c47b13ad06e
Anton <antonkosicyn@rambler.ru>
2021-07-09 19:25:05 +0300

    поменял обратно nextcardtn

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sentence = document.getElementById("sentence")
 const buttonElements = document.getElementsByTagName('button')
 
 const answerBtn = document.getElementById("answer-stop")
-const nextCardBtn = document.getElementById("next-card")
+const nextcardBtn = document.getElementById("next-card")
 const loader = document.getElementById("loader")
 const cardContainer = document.getElementById("card-container")
 const playBtn = document.getElementById("play-pause")
@@ -81,6 +81,6 @@ function generateWord() {
 generateWord()
 
 answerBtn.addEventListener("click", showAnswer)
-nextCardBtn.addEventListener("click", generateWord)
+nextcardBtn.addEventListener("click", generateWord)
 playBtn.addEventListener("click", playTrack)
 audioElement.addEventListener('ended', toggleButton)
